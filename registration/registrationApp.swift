98d20e277d70430362b317f6721650a9099a5783
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-19 17:42:04 +0530

    Initial Commit

---
+++ registration/registrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  registrationApp.swift
+//  registration
+//
+//  Created by Shaan on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct registrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
